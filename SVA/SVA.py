3788118c2dba27e26d96c33ef913fe7a274e3b1c
James Taylor <Jamesetay1@gmail.com>
2020-11-14 23:26:50 -0600

    svaup

--- SVA/SVA.py
+++ SVA/SVA.py
@@ -4,7 +4,6 @@ nlp = stanza.Pipeline('en', processors = "tokenize, pos, lemma, depparse", batch
 
 with open('testsentences.txt', 'r') as file:
     data = file.read()
-print(data)
 doc = nlp(data)
 
 nsubj_Agreement_dict = {
@@ -12,8 +11,8 @@ nsubj_Agreement_dict = {
                 "VBD": ["NN", "NNS", "NNP", "NNPS", "PRP_1", "PRP_2", "DT_1", "DT_2"],
                 "VBG": ["NN", "NNS", "NNP", "NNPS", "PRP_1", "PRP_2", "DT_1", "DT_2"],
                 "VBN": ["NN", "NNS", "NNP", "NNPS", "PRP_1", "PRP_2", "DT_1", "DT_2"],
-                "VBZ": ["NN", "NNP", "PRP_2", "DT_2"],
-                "VBP": ["NNS", "NNPS", "PRP_1", "DT_1"],
+                "VBZ": ["NN", "NNP", "PRP_2", "DT_1"],
+                "VBP": ["NNS", "NNPS", "PRP_1", "DT_2"],
 }
 
 grouping_dict = {
@@ -23,6 +22,7 @@ grouping_dict = {
                 "DT_2": ["these", "those"],
 }
 def error(dep, gov):
+    print(dep.text + "," + gov.text)
     if dep.xpos in nsubj_Agreement_dict[gov.xpos]:
         return False;
 
@@ -35,24 +35,29 @@ def error(dep, gov):
 # ACCEPTABLE COMBINATIONS HERE
 correct = []
 incorrect = []
+compounds = []
 for sent in doc.sentences:
     for word in sent.words:
-        if word.deprel == "nsubj":
-            dep = word
-            dep_pos = word.xpos
+        dep = word
+        dep_pos = word.xpos
 
-            gov = sent.words[word.head-1]
-            gov_pos = gov.xpos
+        gov = sent.words[word.head - 1]
+        gov_pos = gov.xpos
+
+        if word.deprel == "nsubj":
             if gov_pos not in nsubj_Agreement_dict:
                 continue
 
-            # print("Checking " + word.text + "(" + word.xpos + ") " +
-            #       "<--nsubj--- "
-            #       + sent.words[word.head-1].text + "(" + gov_pos+")")
             if error(dep, gov):
                 incorrect.append(word.text + "(" + word.xpos + ") " + "<--nsubj--- " + sent.words[word.head-1].text + "(" + gov_pos + ")")
             else:
                 correct.append(word.text + "(" + word.xpos + ") " + "<--nsubj--- " + sent.words[word.head-1].text + "(" + gov_pos + ")")
+
+        if word.deprel == "compound":
+            if gov_pos == dep_pos:
+                compounds.append(word.text + "(" + word.xpos + ") " + "<--compound--- " + sent.words[word.head-1].text + "(" + gov_pos + ")")
+
+
 
 total_num = len(correct) + len(incorrect)
 correct_percent = len(correct)/total_num * 100
@@ -67,3 +72,6 @@ print(*incorrect,sep='\n')
 
 print("\nThe following nsubj dependencies were found to be correct:")
 print(*correct,sep='\n')
+
+print("\nThe following compound nouns were found and could actually be subject verb agreement errors:")
+print(*compounds,sep='\n')
